b3d5134c28e99f226615bd7e7d0c3bee5fba0ea4
Kevin Diaz <kevinadg@gmail.com>
2018-06-07 14:35:12 -0500

    Update on basic form
    addition of elements due to loss in local progress

--- Tutorials/BasicForm/BasicForm/Form1.Designer.cs
+++ Tutorials/BasicForm/BasicForm/Form1.Designer.cs
@@ -28,13 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.checkC = new System.Windows.Forms.CheckBox();
+            this.checkASP = new System.Windows.Forms.CheckBox();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.rdMale = new System.Windows.Forms.RadioButton();
+            this.rdFemale = new System.Windows.Forms.RadioButton();
+            this.listCity = new System.Windows.Forms.ListBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtAddress);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.labelAddress);
+            this.groupBox1.Controls.Add(this.labelName);
+            this.groupBox1.Location = new System.Drawing.Point(39, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Details";
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(6, 57);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(45, 13);
+            this.labelAddress.TabIndex = 1;
+            this.labelAddress.Text = "Address";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(6, 28);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Name";
+            // 
+            // checkC
+            // 
+            this.checkC.AutoSize = true;
+            this.checkC.Location = new System.Drawing.Point(296, 155);
+            this.checkC.Name = "checkC";
+            this.checkC.Size = new System.Drawing.Size(40, 17);
+            this.checkC.TabIndex = 1;
+            this.checkC.Text = "C#";
+            this.checkC.UseVisualStyleBackColor = true;
+            // 
+            // checkASP
+            // 
+            this.checkASP.AutoSize = true;
+            this.checkASP.Location = new System.Drawing.Point(296, 195);
+            this.checkASP.Name = "checkASP";
+            this.checkASP.Size = new System.Drawing.Size(47, 17);
+            this.checkASP.TabIndex = 1;
+            this.checkASP.Text = "ASP";
+            this.checkASP.UseVisualStyleBackColor = true;
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(296, 267);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(75, 23);
+            this.buttonSubmit.TabIndex = 2;
+            this.buttonSubmit.Text = "Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            // 
+            // rdMale
+            // 
+            this.rdMale.AutoSize = true;
+            this.rdMale.Location = new System.Drawing.Point(296, 55);
+            this.rdMale.Name = "rdMale";
+            this.rdMale.Size = new System.Drawing.Size(48, 17);
+            this.rdMale.TabIndex = 3;
+            this.rdMale.TabStop = true;
+            this.rdMale.Text = "Male";
+            this.rdMale.UseVisualStyleBackColor = true;
+            // 
+            // rdFemale
+            // 
+            this.rdFemale.AutoSize = true;
+            this.rdFemale.Location = new System.Drawing.Point(296, 87);
+            this.rdFemale.Name = "rdFemale";
+            this.rdFemale.Size = new System.Drawing.Size(59, 17);
+            this.rdFemale.TabIndex = 3;
+            this.rdFemale.TabStop = true;
+            this.rdFemale.Text = "Female";
+            this.rdFemale.UseVisualStyleBackColor = true;
+            // 
+            // listCity
+            // 
+            this.listCity.FormattingEnabled = true;
+            this.listCity.Items.AddRange(new object[] {
+            "Puebla",
+            "Cholula",
+            "Berlin",
+            "Tokyo",
+            "Moscú",
+            "Nairobi",
+            "Hesinki"});
+            this.listCity.Location = new System.Drawing.Point(39, 155);
+            this.listCity.Name = "listCity";
+            this.listCity.Size = new System.Drawing.Size(120, 95);
+            this.listCity.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(65, 28);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(122, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(65, 57);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(122, 20);
+            this.txtAddress.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(413, 323);
+            this.Controls.Add(this.listCity);
+            this.Controls.Add(this.rdFemale);
+            this.Controls.Add(this.rdMale);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.checkASP);
+            this.Controls.Add(this.checkC);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.CheckBox checkC;
+        private System.Windows.Forms.CheckBox checkASP;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.RadioButton rdMale;
+        private System.Windows.Forms.RadioButton rdFemale;
+        private System.Windows.Forms.ListBox listCity;
     }
 }
 
--- Tutorials/BasicForm/BasicForm/Form1.cs
+++ Tutorials/BasicForm/BasicForm/Form1.cs
@@ -16,5 +16,7 @@ namespace BasicForm
         {
             InitializeComponent();
         }
+
+        
     }
 }
